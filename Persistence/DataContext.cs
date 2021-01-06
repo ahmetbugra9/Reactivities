24533fc6c8ff1fa531118cd9e8883806309d5e0c
ahmetbugra9 <ahmetbugrasahin9@gmail.com>
2021-01-06 13:21:59 +0300

    üçüncü

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -10,6 +10,7 @@ namespace Persistence
         {
         }
         public DbSet<Value> Values { get; set; }
+        public DbSet<Activity> Activities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -21,4 +22,5 @@ namespace Persistence
                 );
         }
     }
+
 }
--- domain/Value.cs
+++ domain/Value.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace domain
+﻿namespace domain
 {
     public class Value
     {
